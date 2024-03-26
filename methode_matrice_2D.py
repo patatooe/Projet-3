562553b06ab87aaf5f4bd80f0745503c5abb6eec
francisbpolymtl <francis.blais@polymtl.ca>
2024-03-25 21:51:56 -0400

    Code matrice2D complet et fonctionnel

--- methode_matrice_2D.py
+++ methode_matrice_2D.py
@@ -4,66 +4,71 @@ import matplotlib.pyplot as plt
 
 def methode_matrice_2D(C_p, K, rho, tau, Q_0, T_s, d_pS, p, l_x, l_z, Lx, Lz, temps, d):
 
+     Nx=int(np.rint(Lx/d+1)) # Nombre de nœuds le long de X
+     Nz=int(np.rint(Lz/d+1)) # Nombre de nœuds le long de Z
+     A=np.zeros((Nx*Nz,Nx*Nz),dtype=np.double)
+     b=np.zeros((Nx*Nz,1),dtype=np.double)
+
+     
      sigma = 5.67e-8 # (W/(m^2K^4)) Constante de Stefan-Boltzmann
      def Aindex(i,j): #Associé la case i,j à sa colone dans la matrice M
-          index=(j-1)+i
+          index=(j-1)*Nz+i
           return index-1
      
      def St(Q_0, d_pS, tau, temps):
         S = Q_0*(1+np.cos(temps/tau))
+        return S
 
-     Nx=int(np.rint(Lx/d+1)) # Nombre de nœuds le long de X
-     Ny=int(np.rint(Lz/d+1)) # Nombre de nœuds le long de Z
-     A=np.zeros((Nx*Ny,Nx*Ny),dtype=np.double)
-     b=np.zeros((Nx*Ny,1),dtype=np.double)
-
-
-     for i in np.arange(1,Ny+1,1): #i=1,..,Ny - numérotation des nœuds sur un maillage physique
+     for i in np.arange(1,Nz+1,1): #i=1,..,Ny - numérotation des nœuds sur un maillage physique
         z=np.round((i-1)*d, decimals=12)
         for j in np.arange(1,Nx+1,1): #j=1,..,Nx - numérotation des nœuds sur un maillage physique
             x=np.round((j-1)*d, decimals=12)
 
-        # Condition frontière en x==0 (T'(0) = 0)
-        if x == 0 :
-          A[Aindex(i,j),Aindex(i,1)] = -3
-          A[Aindex(i,j),Aindex(i,2)] = 4
-          A[Aindex(i,j),Aindex(i,3)] = -1
-        
-        # Condition frontière en x==Lx (T'(Lx) = 0)
-        elif x == Lx:
-          A[Aindex(i,j),Aindex(i,1)] = -3
-          A[Aindex(i,j),Aindex(i,2)] = 4
-          A[Aindex(i,j),Aindex(i,3)] = -1              
 
-        # Condition frontière en z==0
-        elif z == 0:
-          A[Aindex(i,j),Aindex(i,1)] = -(K/(2*d)*sigma*T_s^3)
-          A[Aindex(i,j),Aindex(i,2)] = 4
-          A[Aindex(i,j),Aindex(i,3)] = -1
-          b[Aindex(i,j)] = -K*St(Q_0, d_pS, tau, temps)/(2*d)
+            Aij = Aindex(i,j)
 
-        # Condition frontière en z==Lz
-        elif z == Lz:
-          A[Aindex(i,j), Aindex(i,j)] = 1    
-          b[Aindex(i,j)] = T_s
-        
-        # Temperature à 394 K pour tous les points dans l'abris
-        elif z > p  and z < l_z+p and x < l_x/2:
-          A[Aindex(i,j), Aindex(i,j)] = 1
-          b[Aindex(i,j), Aindex(i,j)] = 394 
+            # Condition frontière en z==0
+            if i == 1:
+              A[Aindex(i,j),Aindex(i,j)] = -(3+2*d/K*T_s**3)
+              A[Aindex(i,j),Aindex(i+1,j)] = 4
+              A[Aindex(i,j),Aindex(i+2,j)] = -1
+              b[Aindex(i,j)] = -(2*d)*St(Q_0, d_pS, tau, temps)/(K)
 
-        # Tous les autres points
-        elif x > 0 and x < Lx and z > 0 and z < Lz  : 
-          A[Aindex(i,j),Aindex(i,j-1)] = 1
-          A[Aindex(i,j),Aindex(i,j+1)] = 1
-          A[Aindex(i,j),Aindex(i,j)] = -4
-          A[Aindex(i,j),Aindex(i-1,j)] = 1
-          A[Aindex(i,j),Aindex(i+1,j)] = 1
+            # Condition frontière en z==Lz
+            elif i == Nz:
+              A[Aindex(i,j), Aindex(i,j)] = 1    
+              b[Aindex(i,j)] = T_s
+            # Condition frontière en x==0 (T'(0) = 0)
+            elif j == 1 :
+              A[Aindex(i,j),Aindex(i,j)] = -3
+              A[Aindex(i,j),Aindex(i,j+1)] = 4
+              A[Aindex(i,j),Aindex(i,j+2)] = -1
+
+            # Condition frontière en x==Lx (T'(Lx) = 0)
+            elif j == Nx:
+              A[Aindex(i,j),Aindex(i,j)] = 3
+              A[Aindex(i,j),Aindex(i,j-1)] = -4
+              A[Aindex(i,j),Aindex(i,j-2)] = 1              
+
+            # Temperature à 394 K pour tous les points dans l'abris
+            elif i > p/d+1  and i < (l_z+p)/d+1 and j < l_x/(2*d)+1:
+              A[Aindex(i,j), Aindex(i,j)] = 1
+              b[Aindex(i,j)] = 394 
+
+            # Tous les autres points
+            elif j > 1 and j < Nx and i > 1 and i < Nz  : 
+              A[Aindex(i,j),Aindex(i,j-1)] = 1
+              A[Aindex(i,j),Aindex(i,j+1)] = 1
+              A[Aindex(i,j),Aindex(i,j)] = -4
+              A[Aindex(i,j),Aindex(i-1,j)] = 1
+              A[Aindex(i,j),Aindex(i+1,j)] = 1
+            else :
+               print('indice ne rentre dans aucune catégorie')
      
      
-     return A, b, x, z, Nx, Ny
+     return A, b, x, z, Nx, Nz
 
-A, b, x, z, Nx, Ny = methode_matrice_2D(C_p=675, K=1, rho=2000, tau=12, Q_0=492, T_s=288.15, d_pS=1, p=3, l_x=3, l_z=3, Lx=10, Lz=10, temps = 0, d =10)
+A, b, x, z, Nx, Ny = methode_matrice_2D(C_p=675, K=1, rho=2000, tau=12, Q_0=492, T_s=288.15, d_pS=1, p=3, l_x=3, l_z=3, Lx=4, Lz=4, temps = 0, d =1)
 
 T=np.zeros((Nx*Ny,1),dtype=np.double)
 Tr=np.zeros((Ny,Nx),dtype=np.double)
