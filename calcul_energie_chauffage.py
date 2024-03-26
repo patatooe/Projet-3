05d2a9e6466b82f96cd7a450b510da546a20d5e7
Sarazing <erdna.sarazin@gmail.com>
2024-03-26 17:44:06 -0400

    Code flux thermique
    Energie necessaire

--- calcul_energie_chauffage.py
+++ calcul_energie_chauffage.py
@@ -1,3 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
-def calcul_energie_chauffage():
-     return 1
+def calcul_energie_chauffage(lbd, emissivite, Qo, A, T_surf, T_grad, dt,t):
+     
+     #flux rayonnemment 
+     alpha = 5.6703*10**(-8)
+     F_ray = emissivite * alpha * T_surf
+     
+     #flux conduction 
+     F_cond = lbd * T_grad
+     
+     #flux du Soleil
+     Flux_soleil = Qo + A*np.sin(2*np.pi*t/dt)
+     
+     #Flux total entrant
+     F_entrant = F_ray + F_cond + Flux_soleil
+     
+     #Energie par unite de surface
+     E = F_entrant * dt
+     
+     
+     return E
+
+
+
